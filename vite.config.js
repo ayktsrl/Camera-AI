6a7c3e58e347eccb49c55ce3fcd170b9f04aa520
ayktsrl <aykutsaral@gmail.com>
2026-04-19 09:45:23 +0300

    11

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+const dahuaAuth = Buffer.from("admin:Geden123").toString("base64");
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0",
+    proxy: {
+      "/camA-mjpeg": {
+        target: "http://10.0.30.101",
+        changeOrigin: true,
+        secure: false,
+        headers: {
+          Authorization: `Basic ${dahuaAuth}`,
+        },
+        rewrite: () => "/cgi-bin/mjpg/video.cgi?channel=1&subtype=1",
+      },
+    },
+  },
+});
